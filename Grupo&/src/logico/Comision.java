b068f701d65ed7d9edb9cdfe4caeaf727bc381bd
reymer471 <reymerpolanco471@gmail.com>
2024-11-26 13:45:57 -0500

    Comsion actualizada agregando construtore and setter and getter

--- Grupo&/src/logico/Comision.java
+++ Grupo&/src/logico/Comision.java
@@ -10,4 +10,32 @@ private ArrayList<Jurado>losJurados;
 
 
 
+public Comision(String nombreComision, String codigo, ArrayList<Jurado> losJurados) {
+	super();
+	this.nombreComision = nombreComision;
+	this.codigo = codigo;
+	this.losJurados = losJurados;
+	
+}
+public String getNombreComision() {
+	return nombreComision;
+}
+public void setNombreComision(String nombreComision) {
+	this.nombreComision = nombreComision;
+}
+public String getCodigo() {
+	return codigo;
+}
+public void setCodigo(String codigo) {
+	this.codigo = codigo;
+}
+public ArrayList<Jurado> getLosJurados() {
+	return losJurados;
+}
+public void setLosJurados(ArrayList<Jurado> losJurados) {
+	this.losJurados = losJurados;
+}
+
+
+
 }
