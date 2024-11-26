f61de9d0ad1a33e398a4ad5dd0e6a089351c8645
reymer471 <reymerpolanco471@gmail.com>
2024-11-26 13:52:12 -0500

    actualizacion de clases

---
+++ Grupo&/src/logico/Participante.java
@@ -0,0 +1,29 @@
+package logico;
+
+public class Participante extends Persona {
+	private String direccion;
+	private String email;
+	
+	public Participante(String codigo, String nombre, String apellido, String direccion, String email) {
+		super(codigo, nombre, apellido);
+		this.direccion = direccion;
+		this.email = email;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ Grupo&/src/logico/Reporte.java
@@ -0,0 +1,34 @@
+package logico;
+
+import java.util.Date;
+
+public class Reporte extends infoEvento {
+	private String tipoReporte; 
+	private Date generacion;
+	
+	public Reporte(String temaEvento, String numeroPersonas, String duracionEvento, String tipoReporte,
+			Date generacion) {
+		super(temaEvento, numeroPersonas, duracionEvento);
+		this.tipoReporte = tipoReporte;
+		this.generacion = generacion;
+	}
+
+	public String getTipoReporte() {
+		return tipoReporte;
+	}
+
+	public void setTipoReporte(String tipoReporte) {
+		this.tipoReporte = tipoReporte;
+	}
+
+	public Date getGeneracion() {
+		return generacion;
+	}
+
+	public void setGeneracion(Date generacion) {
+		this.generacion = generacion;
+	}
+	
+	
+
+}
