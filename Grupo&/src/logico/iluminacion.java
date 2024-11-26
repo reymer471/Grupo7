03d1d738a00f70548cd9b91fa47a3954f4ad5b29
reymer471 <reymerpolanco471@gmail.com>
2024-11-26 14:19:46 -0500

    termine de agregar todas las clases por ahora

---
+++ Grupo&/src/logico/iluminacion.java
@@ -0,0 +1,19 @@
+package logico;
+
+public class iluminacion {
+	private String tipoDeiluminacion;
+
+	public iluminacion(String tipoDeiluminacion) {
+		super();
+		this.tipoDeiluminacion = tipoDeiluminacion;
+	}
+
+	public String getTipoDeiluminacion() {
+		return tipoDeiluminacion;
+	}
+
+	public void setTipoDeiluminacion(String tipoDeiluminacion) {
+		this.tipoDeiluminacion = tipoDeiluminacion;
+	}
+	
+}
