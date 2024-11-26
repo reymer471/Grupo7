c1164c2f42ddc7745ec19a33fcc9ce5666352e97
reymer471 <reymerpolanco471@gmail.com>
2024-11-26 13:41:50 -0500

    Clase jurado

---
+++ Grupo&/src/logico/Jurado.java
@@ -0,0 +1,13 @@
+package logico;
+
+public class Jurado extends Persona {
+	private String experiencia;
+	
+	public Jurado(String codigo, String nombre, String apellido) {
+		super(codigo, nombre, apellido);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
