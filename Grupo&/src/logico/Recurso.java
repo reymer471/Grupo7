b96dc3df5ef6e7325136c512f3f43487e19fe244
ASUS <ASUS@AbBlue>
2024-11-30 00:17:56 -0400

    Actualizando clase recurso

--- Grupo&/src/logico/Recurso.java
+++ Grupo&/src/logico/Recurso.java
@@ -1,43 +1,49 @@
 package logico;
 
-public abstract class Recurso {
+public class Recurso {
 	
- protected String id ;
- protected String nombre;
- protected int cantidadTotal;
+	 private String id ;
+	 private String nombre;
+	 private int cantidadTotal;
+	 private String tiporecurso;
  
-public Recurso(String id, String nombre, int cantidadTotal) {
-	super();
-	this.id = id;
-	this.nombre = nombre;
-	this.cantidadTotal = cantidadTotal;
-}
+	public Recurso(String id, String nombre, int cantidadTotal) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidadTotal = cantidadTotal;
+	}
 
-public String getId() {
-	return id;
-}
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getCantidadTotal() {
+		return cantidadTotal;
+	}
+	
+	public void setCantidadTotal(int cantidadTotal) {
+		this.cantidadTotal = cantidadTotal;
+	}
 
-public void setId(String id) {
-	this.id = id;
-}
+	public String getTiporecurso() {
+		return tiporecurso;
+	}
 
-public String getNombre() {
-	return nombre;
-}
-
-public void setNombre(String nombre) {
-	this.nombre = nombre;
-}
-
-public int getCantidadTotal() {
-	return cantidadTotal;
-}
-
-public void setCantidadTotal(int cantidadTotal) {
-	this.cantidadTotal = cantidadTotal;
-}
- 
-
-
+	public void setTiporecurso(String tiporecurso) {
+		this.tiporecurso = tiporecurso;
+	}
 
 }
