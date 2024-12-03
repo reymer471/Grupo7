525a53c76b61c2db9dab74c4425088cdf1048695
user <user@DESKTOP-7VH196C>
2024-12-03 09:46:54 -0500

    agragando cambios visuales a principal

--- Grupo&/src/visual/Principal.java
+++ Grupo&/src/visual/Principal.java
@@ -8,6 +8,7 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.BevelBorder;
+import javax.swing.border.EtchedBorder;
 
 public class Principal extends JFrame {
     
@@ -27,24 +28,31 @@ public class Principal extends JFrame {
     }
 
     public Principal() {
+    	setTitle("Gestor de eventos");
     	setIconImage(Toolkit.getDefaultToolkit().getImage(Principal.class.getResource("/javax/swing/plaf/metal/icons/ocean/paletteClose-pressed.gif")));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 600, 400);
+        setBounds(50, 100, 521, 363);
         Dimension dim = getToolkit().getScreenSize();
-        setSize(dim.width+10,dim.height-40);
+        //setSize(dim.width+10,dim.height-40);
         setLocationRelativeTo(null);
         JMenuBar menuBar= new JMenuBar();
         setJMenuBar(menuBar);
         
         // Panel principal
         contentPane = new JPanel();
+        contentPane.setBorder(new EtchedBorder(EtchedBorder.LOWERED, Color.BLUE, Color.YELLOW));
         setContentPane(contentPane);
-        contentPane.setLayout(new BorderLayout(0, 0));
         
         // Barra de menú
         JMenuBar menuBar1 = new JMenuBar();
         menuBar1.setBackground(UIManager.getColor("Button.darkShadow"));
         setJMenuBar(menuBar1);
+        
+        JMenu mnNewMenu = new JMenu("Clientes");
+        menuBar1.add(mnNewMenu);
+        
+        JMenu mnNewMenu_1 = new JMenu("Administracion");
+        menuBar1.add(mnNewMenu_1);
         
         // Menú Archivo
         JMenu mnArchivo = new JMenu("Emergencia");
@@ -59,19 +67,25 @@ public class Principal extends JFrame {
             }
         });
         mnArchivo.add(mntmSalir);
+        contentPane.setLayout(null);
         
 
       
         
      // Panel central con mensaje de bienvenida
         JPanel panelCentral = new JPanel();
-        panelCentral.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-        panelCentral.setLayout(new FlowLayout(FlowLayout.CENTER));  
-        contentPane.add(panelCentral, BorderLayout.CENTER);
+        panelCentral.setBounds(0, 0, 505, 303);
+        panelCentral.setBorder(new EtchedBorder(EtchedBorder.LOWERED, Color.BLUE, Color.YELLOW));
+        contentPane.add(panelCentral);
+        panelCentral.setLayout(null);
 
         // Mensaje de bienvenida
-        JLabel lblNewLabel = new JLabel("Bienvenidos al sistema de Eventos  ");
+        JLabel lblNewLabel = new JLabel("Bienvenidos al sistema de Eventos de la PUCMM  ");
+        lblNewLabel.setBounds(117, 8, 271, 32);
         lblNewLabel.setIcon(new ImageIcon(Principal.class.getResource("/com/sun/java/swing/plaf/windows/icons/Inform.gif")));
-        panelCentral.add(lblNewLabel); 
+        panelCentral.add(lblNewLabel);
+        
+        ImageIcon ico = new ImageIcon(getClass().getResource("Marca PUCMM (Color).png"));
+        ImageIcon img = new ImageIcon(ico.getImage().getScaledInstance(lblNewLabel.getWidth(), lblNewLabel.getHeight(), Image.SCALE_SMOOTH));
     }
 }
