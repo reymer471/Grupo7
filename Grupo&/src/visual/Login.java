e1225a2fd03d6af7a56527371de53d4f82282150
ASUS <ASUS@AbBlue>
2024-12-05 13:02:28 -0400

    Arreglando Login

--- Grupo&/src/visual/Login.java
+++ Grupo&/src/visual/Login.java
@@ -12,7 +12,7 @@ import javax.swing.border.EtchedBorder;
 
 import org.omg.PortableInterceptor.USER_EXCEPTION;
 
-import com.sun.java.util.jar.pack.Package.File;
+//import com.sun.java.util.jar.pack.Package.File;
 
 import logico.Control;
 import logico.Usuario;
@@ -75,6 +75,9 @@ public class Login extends JFrame {
 					}catch (IOException e1) {
 					}
 				}catch(IOException e) {
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 				
 				/*try {
