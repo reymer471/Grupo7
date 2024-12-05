a75ea42463701964399c39c652a8abfc077824cd
ASUS <ASUS@AbBlue>
2024-12-05 07:48:55 -0400

    Lista Registrar Tc

--- Grupo&/src/visual/RegistrarTC.java
+++ Grupo&/src/visual/RegistrarTC.java
@@ -2,15 +2,40 @@ package visual;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+import logico.Comision;
+import logico.Participante;
+import logico.SPEC;
+import logico.TrabajoCientifico;
+
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RegistrarTC extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField txtTItuloTc;
+	private JTextField txtCodigoTc;
+	private JTextField txtEntradaParticipante;
+	private ArrayList<String> opcionescomision = new ArrayList<>();
+	private JComboBox cbxComision;
+	
+	private ArrayList<Participante> participantesAux;
+	private JTextField txtDescripTc;
 
 	/**
 	 * Launch the application.
@@ -29,27 +54,186 @@ public class RegistrarTC extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegistrarTC() {
+		setTitle("Registrar Trabajo Cientifico");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+			contentPanel.add(panel, BorderLayout.CENTER);
+			panel.setLayout(new BorderLayout(0, 0));
+			{
+				JPanel panel_1 = new JPanel();
+				panel_1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+				panel.add(panel_1, BorderLayout.CENTER);
+				panel_1.setLayout(null);
+				
+				JLabel lblCodigoTc = new JLabel("Codigo:");
+				lblCodigoTc.setBounds(10, 11, 46, 14);
+				panel_1.add(lblCodigoTc);
+				
+				JLabel lblTituloTc = new JLabel("Titulo:");
+				lblTituloTc.setBounds(10, 50, 46, 14);
+				panel_1.add(lblTituloTc);
+				
+				JLabel lblComision = new JLabel("Comision:");
+				lblComision.setBounds(10, 126, 46, 14);
+				panel_1.add(lblComision);
+				
+				cbxComision = new JComboBox();		
+				cbxComision.setBounds(66, 123, 348, 20);
+				panel_1.add(cbxComision);
+				
+				txtTItuloTc = new JTextField();
+				txtTItuloTc.setBounds(66, 47, 348, 20);
+				panel_1.add(txtTItuloTc);
+				txtTItuloTc.setColumns(10);
+				
+				txtCodigoTc = new JTextField();
+				txtCodigoTc.setText("TC-" + SPEC.getInstance().codTrajCientifico);
+				txtCodigoTc.setEditable(false);
+				txtCodigoTc.setBounds(66, 8, 86, 20);
+				panel_1.add(txtCodigoTc);
+				txtCodigoTc.setColumns(10);
+				
+				JLabel lblNewLabel = new JLabel("Participantes:");
+				lblNewLabel.setBounds(10, 169, 116, 14);
+				panel_1.add(lblNewLabel);
+				
+				JButton btnAgregar = new JButton("Agregar");
+				btnAgregar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(!txtEntradaParticipante.getText().isEmpty()) {
+							Participante participante = SPEC.getInstance().buscarParticipanteById(txtEntradaParticipante.getText());
+							if(participante != null) {
+								anadirParticipanteAAux(participante);
+								
+								javax.swing.JOptionPane.showMessageDialog(
+										null,
+										"Participante agregado exitosamente.",
+										"Información",
+										javax.swing.JOptionPane.INFORMATION_MESSAGE
+									);
+								 cleanEntrada();
+							}
+							
+						}
+					}
+				});
+				btnAgregar.setBounds(325, 165, 89, 23);
+				panel_1.add(btnAgregar);
+				
+				txtEntradaParticipante = new JTextField();
+				txtEntradaParticipante.setBounds(91, 166, 224, 20);
+				panel_1.add(txtEntradaParticipante);
+				txtEntradaParticipante.setColumns(10);
+				
+				JLabel lblDescrip = new JLabel("Descripcion");
+				lblDescrip.setBounds(10, 86, 59, 14);
+				panel_1.add(lblDescrip);
+				
+				txtDescripTc = new JTextField();
+				txtDescripTc.setBounds(66, 83, 348, 20);
+				panel_1.add(txtDescripTc);
+				txtDescripTc.setColumns(10);
+			}
+		}
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
+				JButton okButton = new JButton("Registrar");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						agregarTc();
+					}
+				});
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
+		cleanOpciones();
+		anadirOpcionesComisiones();
+		actualizarComboBox();
+	}
+	
+   
+
+	protected void agregarTc() {
+		// TODO Auto-generated method stub
+		
+		if (txtCodigoTc.getText().isEmpty() || txtDescripTc.getText().isEmpty() || participantesAux.isEmpty() || cbxComision.getSelectedIndex() == 0) {
+			javax.swing.JOptionPane.showMessageDialog(
+				null,
+				"Debe completar todos los campos antes de continuar.",
+				"Error",
+				javax.swing.JOptionPane.ERROR_MESSAGE
+			);
+		
+		}else {
+			Comision comision = SPEC.getInstance().buscarComisionByNombre((String) cbxComision.getSelectedItem());
+			TrabajoCientifico trabajoCientifico = new TrabajoCientifico(
+					txtCodigoTc.getText(), 
+					txtTItuloTc.getText(), 
+					txtDescripTc.getText(), 
+					comision
+			);
+			SPEC.getInstance().insertarTrabajoCientifico(trabajoCientifico);
+			javax.swing.JOptionPane.showMessageDialog(
+					null,
+					"Trabajo Cientifico registrado exitosamente.",
+					"Información",
+					javax.swing.JOptionPane.INFORMATION_MESSAGE
+				);
+		}
+		
+		
 	}
 
+	protected void cleanEntrada() {
+		// TODO Auto-generated method stub
+		txtEntradaParticipante.setText(" ");
+	}
+
+	protected void anadirParticipanteAAux(Participante participante) {
+		// TODO Auto-generated method stub
+		participantesAux.add(participante);
+	}
+
+	private void anadirOpcionesComisiones() {
+        if (!SPEC.getInstance().getMisComisiones().isEmpty()) {
+            for (Comision comision : SPEC.getInstance().getMisComisiones()) {
+                opcionescomision.add(comision.getNombreComision());
+            }
+        } else {
+            opcionescomision.add("No elegida");
+        }
+    }
+
+    private void cleanOpciones() {
+        opcionescomision.clear();
+        opcionescomision.add("<Seleccione>");
+    }
+
+    private void actualizarComboBox() {
+        cbxComision.setModel(new DefaultComboBoxModel<>(new Vector<>(opcionescomision)));
+    }
+    
+   
 }
